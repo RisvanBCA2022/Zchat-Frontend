5345920b85fff2f7b6618a54b94772108bd3ca37
RisvanBCA2022 <mohammedrisvan16@gmail.com>
2024-05-24 14:48:12 +0530

    pathname custom hook add shadcn components added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { TooltipProvider } from "../components/ui/tooltip";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <TooltipProvider>
+
+      {children}
+      </TooltipProvider>
+      </body>
     </html>
   );
 }
